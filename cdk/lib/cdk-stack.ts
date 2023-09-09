811a7537e3a63f02f87876ca10cc01cd410c8a32
zaki3mymy <zaki.last@gmail.com>
2023-09-09 22:18:58 +0900

    build: add dynamodb table and Lambda function

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const tableName = "dropbox2slack-table"
+    const lambdaFunctionName = "dropbox2slack"
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, tableName, {
+      tableName: tableName,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    })
+
+    const lambdaFunction = new lambda.Function(this, lambdaFunctionName, {
+      code: new lambda.AssetCode("../lambda/src"),
+      handler: "lambda_function.lambda_handler",
+      runtime: lambda.Runtime.PYTHON_3_9,
+      environment: {
+        "TABLE_NAME": tableName,
+      }
+    })
   }
 }
