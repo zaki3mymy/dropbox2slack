7ef0cd7fba5a886ae82c4d81ca9d58dd2087ee9b
zaki3mymy <zaki.last@gmail.com>
2023-09-09 23:36:32 +0900

    build: add REST API

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,7 @@ export class CdkStack extends cdk.Stack {
 
     const tableName = "dropbox2slack-table"
     const lambdaFunctionName = "dropbox2slack"
+    const restApiName = "dropbox2slack-api"
 
     const table = new dynamodb.Table(this, tableName, {
       tableName: tableName,
@@ -19,12 +21,21 @@ export class CdkStack extends cdk.Stack {
     })
 
     const lambdaFunction = new lambda.Function(this, lambdaFunctionName, {
-      code: new lambda.AssetCode("../lambda/src"),
+      functionName: lambdaFunctionName,
+      code: new lambda.AssetCode("../src/dropbox2slack"),
       handler: "lambda_function.lambda_handler",
       runtime: lambda.Runtime.PYTHON_3_9,
       environment: {
         "TABLE_NAME": tableName,
       }
     })
+
+    const restApi = new apigateway.RestApi(this, restApiName, {
+      restApiName: restApiName,
+      deployOptions: {
+        stageName: "v1",
+      }
+    })
+    restApi.root.addMethod("GET", new apigateway.LambdaIntegration(lambdaFunction))
   }
 }
