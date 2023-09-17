fc4104462a58a071a0f9ba3a95853ed44c64240d
zaki3mymy <zaki.last@gmail.com>
2023-09-18 07:34:18 +0900

    feat: implements verify method

--- src/dropbox2slack/lambda_function.py
+++ src/dropbox2slack/lambda_function.py
@@ -1,5 +1,15 @@
 import json
 
+def verify(event):
+    challenge = event["queryStringParameters"]["challenge"]
+    headers = {
+        "Content-Type": "text/plain",
+        "X-Content-Type-Options": "nosniff",
+    }
+    return {"statusCode": 200, "headers": headers, "body": challenge}
 
 def lambda_handler(event, context):
+    if event["httpMethod"] == "GET":
+        return verify(event)
+
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
