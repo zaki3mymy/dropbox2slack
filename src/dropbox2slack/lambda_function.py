c51bd8af911ad15eb3a6c5a9087c53afdf45cf2f
zaki3mymy <zaki.last@gmail.com>
2023-09-18 08:55:01 +0900

    revert(webhook): this feature will be implements other PR
    ref: 24d5754ebd8ecee187a2684211ec8b6b8e55d384

--- src/dropbox2slack/lambda_function.py
+++ src/dropbox2slack/lambda_function.py
@@ -1,8 +1,3 @@
-import os
-
-import api.dropboxapi as dropboxapi
-import api.slackapi as slackapi
-import util.models as models
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler.api_gateway import (
     ApiGatewayResolver,
@@ -28,52 +23,6 @@ def verify():
         "X-Content-Type-Options": ["nosniff"],
     }
     return Response(200, headers=headers, body=challenge)
-
-
-@app.post("/")
-def webhook():
-    DROPBOX_TARGET_DIR = os.environ["DROPBOX_TARGET_DIR"]
-    cursor = models.get_cursor()
-
-    res = dropboxapi.list_folder_continue(cursor)
-    models.save_cursor(res["cursor"])
-
-    if len(res["entries"]) == 0:
-        # no change
-        logger.info("no change in {}".format(DROPBOX_TARGET_DIR))
-        return ""
-
-    store = slackapi.ChangedFileStore()
-    for entry in res["entries"]:
-        logger.info(f"entry: {entry}")
-        if entry[".tag"] in ("folder", "deleted"):
-            # process only files
-            continue
-
-        # path_display = "/path/to/folder/<channel name>/<filename>""
-        filepath = entry["path_display"]
-        channel = filepath.replace(DROPBOX_TARGET_DIR + "/", "").split("/")[0]
-
-        try:
-            res = dropboxapi.list_shared_link(filepath)
-            links = res["links"]
-            if len(links) != 0:
-                logger.debug("modify_shared_link")
-                url = links[0]["url"]
-                dropboxapi.modify_shared_link(url)
-            else:
-                logger.debug("create_shared_link")
-                res = dropboxapi.create_shared_link(filepath)
-                logger.debug("Response of create shared link...: {}".format(res))
-                url = res["url"]
-
-            store.add(channel, filepath, url)
-        except Exception as e:
-            logger.exception("shared link error!", e)
-
-    logger.info("send messages...")
-    msg_store = slackapi.generage_messages_store(store)
-    slackapi.send_messages(msg_store)
 
 
 @logger.inject_lambda_context(log_event=True)
